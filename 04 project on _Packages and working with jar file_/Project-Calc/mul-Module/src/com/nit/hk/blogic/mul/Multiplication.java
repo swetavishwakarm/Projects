e959f828dc4b7970222efd1a1c2af4a203885ef4
swetavishwakarm <148953762+swetavishwakarm@users.noreply.github.com>
2024-04-18 15:23:14 +0530

    Delete 04 project on _Packages and working with jar file_ directory

--- 04 project on _Packages and working with jar file_/Project-Calc/mul-Module/src/com/nit/hk/blogic/mul/Multiplication.java
+++
@@ -1,8 +0,0 @@
-package com.nit.hk.blogic.mul;
-
-public class Multiplication {
-	public void mul(int a, int b) {
-		int c = a * b;
-		System.out.printf("\nThe multiplication of %d and %d is: %d", a, b, c);
-	}
-}
