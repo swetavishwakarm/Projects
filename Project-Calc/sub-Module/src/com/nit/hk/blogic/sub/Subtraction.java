cd289b247acfe0cbca6a038857a0a6daf12d1b1f
swetavishwakarm <148953762+swetavishwakarm@users.noreply.github.com>
2024-04-18 14:45:33 +0530

    Delete Project-Calc directory

--- Project-Calc/sub-Module/src/com/nit/hk/blogic/sub/Subtraction.java
+++
@@ -1,8 +0,0 @@
-package com.nit.hk.blogic.sub;
-
-public class Subtraction {
-	public void sub(int a, int b) {
-		int c = a - b;
-		System.out.printf("\nThe subtraction of %d and %d is: %d", a, b, c);
-	}
-}
